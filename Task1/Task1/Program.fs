1dd78ee833fad436ec8b730ba4f5ef2d592877d2
Volodya Petrov <89377379140@mail.ru>
2022-02-25 01:45:10 +0300

    added fibonacci and reverse

--- Task1/Task1/Program.fs
+++ Task1/Task1/Program.fs
@@ -7,6 +7,21 @@ let factorial number =
             factorialHelper (acc * i) (i + 1) number
     factorialHelper 1 1 number
 
-
-let x = factorial 0 
-printfn "%i" x      
+let fibonacci number =
+    let rec fibonacciHelper number acc1 acc2 i =
+        if i >= number then
+            acc1
+        else
+            fibonacciHelper number acc2 (acc1 + acc2) (i + 1)
+    fibonacciHelper number 0 1 1
+    
+let reverse list =
+    let rec reverseHelper list reverseList =
+        if list = [] then
+            reverseList
+        else
+            reverseHelper (List.tail list) ((List.head list)::reverseList)
+            
+    reverseHelper list []
+let x = reverse [1]
+printfn "%A" x      
