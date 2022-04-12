14f34bd2d8f4ddb7af0e7a09d3080691c596c35d
Volodya Petrov <89377379140@mail.ru>
2022-04-12 21:41:48 +0300

    fix network

--- LocalNetwork/LocalNetwork/Network.fs
+++ LocalNetwork/LocalNetwork/Network.fs
@@ -6,22 +6,28 @@ type public Network(matrix: bool[,], computers:Computer[]) =
     let visited = Array.create computers.Length false
     
     let rec runDfs prev current =
+        seq { 
         visited[current] <- true
-        let random = System.Random()
-        if computers[current].Infected = false then
-            while (random.NextSingle() > computers[current].OS.ProbabilityInfection) do
-                printfn "Компьютеру %i не удалось заразить компьютер %i" prev current
-                printfn "Компьютеру %i заразил компьютер %i" prev current
-        computers[current].Infected <- true
-        for i in 0 .. visited.Length - 1 do
-            if visited[i] = false && matrix[current, i] = true then
-                runDfs current i
+        if computers[current].OS.ProbabilityInfection <> 0f then
+            let random = System.Random()
+            if computers[current].Infected = false then
+                while (random.NextSingle() > computers[current].OS.ProbabilityInfection) do
+                    yield ("Компьютеру " + prev.ToString() + " не удалось заразить компьютер " + current.ToString())
+                yield  ("Компьютеру " + prev.ToString() + " заразил компьютер " + current.ToString())
+            computers[current].Infected <- true
+            for i in 0 .. visited.Length - 1 do
+                if i <> current && visited[i] = false && matrix[current, i] = true then
+                    yield! (runDfs current i)
+        }         
     member public this.Computers with get() = computers
         
     member public this.RunInfection () =
-        let mutable startIndex = -1 
-        for i in 0 .. visited.Length - 1 do
-            if computers[i].Infected then
-                startIndex <- i
-        if startIndex <> -1 then
-            runDfs startIndex startIndex
+        seq {
+            let mutable startIndex = -1 
+            for i in 0 .. visited.Length - 1 do
+                if computers[i].Infected then
+                    startIndex <- i
+            if startIndex <> -1 then
+                yield! (runDfs startIndex startIndex)    
+        }
+        
--- LocalNetwork/LocalNetwork/Program.fs
+++ LocalNetwork/LocalNetwork/Program.fs
@@ -1,4 +1,14 @@
 module Program
-
+open LocalNetwork
 // For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+let linux = OS("Linux", 0.5f)
+let windows = OS("Windows", 0.7f)
+let comp1 = Computer(linux)
+let comp2 = Computer(linux)
+let comp3 = Computer(windows)
+let comp4 = Computer(windows)
+comp1.Infected <- true
+let matrix = [ [true; true; false; true]; [true; true; true; false]; [false; true; true; true]; [true; false; true; true] ]
+let network = Network(array2D matrix, [| comp1; comp2; comp3; comp4 |])
+(network.RunInfection()) |> Seq.iter (fun x -> printfn "%s" x)
+
